c1950fb54fc4c1ca48deef1d73f52b3c9c56e71a
MaternaTech <138349261+st-materna-tech@users.noreply.github.com>
2023-07-03 17:24:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,37 +3,21 @@ import sidebar
 import predictions_page
 import data_page_v2
 
-# Set page configuration
 st.set_page_config(layout="wide", page_title="MATERNATECH", page_icon=":baby:")
-st.markdown(
-"""
-<style>
-.sidebar .sidebar-content {
-background-color: #f8f9fa;
-padding: 1rem;
-}
-.logo {
-text-align: center;
-margin-bottom: 2rem;
-}
-.page-title {
-text-align: center;
-font-size: 32px;
-margin-bottom: 2rem;
-}
-</style>
-""",
-unsafe_allow_html=True,
-)
 
-# Logo and page title
-st.markdown('<div class="logo"><h1>MATERNATECH</h1></div>', unsafe_allow_html=True)
-st.markdown('<h2 class="page-title">Pregnancy Journey Companion</h2>', unsafe_allow_html=True)
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 0.5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+        
 
-# Render sidebar and corresponding page
+# st.title("Hello")
 page = sidebar.show()
 
-if page == "Pregnancy Complications Prediction":
-  predictions_page.render_page()
-elif page == "Data Analysis":
-  data_page_v2.render_page()
+if page=="Pregnancy Complications Prediction":
+    predictions_page.render_page()
+elif page=="Data Analysis":
+    data_page_v2.render_page()
