b89a0ef6d402704aea4afbb5ad9d0ee0a5cc08db
MaternaTech <138349261+st-materna-tech@users.noreply.github.com>
2023-07-03 17:29:18 +0100

    Update predictions_page.py

--- predictions_page.py
+++ predictions_page.py
@@ -53,6 +53,7 @@ def predict_sentiment(text): #, model_type):
 def render_page():
     st.title("MATERNATECH")
     st.markdown("---")
+    
     st.subheader("Pregnancy Complications Prediction")
     st.text("Your trusted companion for a healthy pregnancy journey")
     st.text("")
